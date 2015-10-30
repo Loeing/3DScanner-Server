0c72e1e1d945369d366d4b285563188e830e3dde
Loeing <jlt81@case.edu>
2015-10-30 09:46:59 -0400

    receiving videogit add --allgit add --all

--- server.py
+++ server.py
@@ -1,7 +1,8 @@
 import socket
+import pdb
 
 s= socket.socket()
-port = 8081
+port = 8080
 s.bind(('',port))
 f = open('stored.mp4','wb')
 print 'waiting'
@@ -9,8 +10,10 @@ s.listen(5)
 while True:
     c, addr = s.accept()
     print 'Connected to', addr
+    f = open('stored.mp4','wb')
     packet = c.recv(1024)
     while(packet):
+        #pdb.set_trace()
         f.write(packet)
         packet = c.recv(1024)
     f.close()
