c401dd90b708895e607052058d5d15d4045d174c
Loeing <jlt81@case.edu>
2015-11-13 01:36:57 -0500

    added ros automation to the server

--- server.py
+++ server.py
@@ -1,5 +1,7 @@
 import socket
 import pdb
+import rostopic
+import subprocess
 from image_converter import img_converter
 from purge import purge
 
@@ -23,7 +25,20 @@ while True:
     c.close()
     purge()
     img_converter('stored.mp4')
-
+    #check that ROS is running on the server
+    try:
+        rostopic.rosgraph.Master('/rostopic').getPid()
+    except socket.error:
+        try:
+            subprocess.Popen(['roslaunch', 'ORB_SLAM/ExampleGroovyOrNewer.launch'])
+        except error:
+            raise rostopic.ROSTopicIOException("can't communicate with server")
+    try:
+        subprocess.Popen(['rosrun', 'BagFromImages', 'BagFromImages', 'images/', '.png', '30', 'ORB_SLAM/Data/Example.bag']) 
+        subprocess.Popen(['rosbag', 'play', 'ORB_SLAM/Data/Example.bag'])
+    except error:
+        raise rostopic.ROSTopicIOException("can't run ORB_SLAM")
+        
 
 
 
